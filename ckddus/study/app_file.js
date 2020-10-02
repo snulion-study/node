dd46c092e7258e2ead290c19a6e7d77939354d32
cypark <blackco66@gmail.com>
2020-10-03 03:30:14 +0900

    mongodb 실습

--- ckddus/study/app_file.js
+++ ckddus/study/app_file.js
@@ -1,10 +1,32 @@
 // 웹 어플리케이션을 "file"에 저장하는 방식으로 구성.
 var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
 var fs = require('fs');
-var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+const config = require('./config/key');
+const mongoose = require('mongoose');
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('MongoDB connected...'))
+  .catch((err) => console.log(err));
+const { User } = require('./models/User');
 app.locals.pretty = true;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 app.set('views', 'views_file');
 app.set('view engine', 'jade');
 app.get('/topic/new', function (req, res) {
